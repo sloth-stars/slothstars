ca2b611f13e973be902db4f561b3f85795343054
franklin <franklin@weareinteractive.com>
2014-04-23 10:27:06 +0200

    renamed src folder

--- lib/CliCall.php
+++ lib/CliCall.php
@@ -24,17 +24,34 @@ class CliCall
    * @var string
    */
   private $cmd;
+	/**
+	 * @var array
+	 */
+	private $arguments;
+	/**
+	 * @var array
+	 */
+	private $envVars;
+	/**
+	 * @var string
+	 */
+	private $cwd = null;
 
   // --------------------------------------------------------------------------------------------
   // ~ Constructor
   // --------------------------------------------------------------------------------------------
 
-  /**
-   * @param string $cmd
-   */
-  public function __construct($cmd)
+	/**
+	 * @param string $cmd
+	 * @param array $arguments
+	 * @param array $envVars
+	 */
+	public function __construct($cmd, $arguments=array(), $envVars=array())
   {
     $this->cmd = $cmd;
+	  $this->cmd = self::resolveCommand($cmd);
+	  $this->arguments = $arguments;
+	  $this->envVars = $envVars;
   }
 
   // --------------------------------------------------------------------------------------------
@@ -65,12 +82,42 @@ class CliCall
     return $this->exitStatus;
   }
 
+	/**
+	 * @param array $envVars
+	 * @return $this
+	 */
+	public function addEnvVars(array $envVars)
+	{
+		$this->envVars = array_merge($this->envVars, $envVars);
+		return $this;
+	}
+
+	/**
+	 * @param array $arguments
+	 * @return $this
+	 */
+	public function addArguments(array $arguments)
+	{
+		$this->arguments = array_merge($this->arguments, $arguments);
+		return $this;
+	}
+
+	/**
+	 * @param string $cwd
+	 * @return $this
+	 */
+	public function setCwd($cwd)
+	{
+		$this->cwd = $cwd;
+		return $this;
+	}
+
   /**
    * execute the command line call
    *
    * @return \SlothStars\CliCall
    */
-  public function execute($cwd=null, $envVars=array())
+  public function execute()
   {
       $stdOut = '';
       $stdErr = '';
@@ -83,7 +130,7 @@ class CliCall
       );
 
       # open process
-      $process = proc_open($this->cmd, $descriptorSpec, $pipes, $cwd, $envVars);
+      $process = proc_open($this->renderCommand(), $descriptorSpec, $pipes, $this->cwd, $this->envVars);
 
       # validate
       if (!is_resource($process)) trigger_error('could not spawn process', E_USER_ERROR);
@@ -129,16 +176,50 @@ class CliCall
     if ($bytes !== false && !empty($bytes)) $target .= $bytes;
   }
 
+
+	/**
+	 * render the command string
+	 *
+	 * @return string
+	 */
+	public function renderCommand()
+	{
+		$cmd = $this->cmd;
+		$noescape = array('<', '>', '|');
+		foreach ($this->arguments as $arg) {
+			$cmd .= ' ' . ((\in_array($arg, $noescape)) ? $arg : escapeshellarg($arg));
+		}
+		return $cmd;
+	}
+
   //---------------------------------------------------------------------------------------------
   // ~ Public static methods
   //---------------------------------------------------------------------------------------------
 
   /**
    * @param string $cmd
+   * @param array $arguments
+   * @param array $envVars
    * @return \SlothStars\CliCall
    */
-  public static function create($cmd)
+  public static function create($cmd, $arguments=array(), $envVars=array())
   {
-    return new self($cmd);
+    return new self($cmd, $arguments, $envVars);
   }
+
+	/**
+	 * @param string $cmd
+	 * @return bool|string
+	 */
+	public static function resolveCommand($cmd)
+	{
+		$ret = '';
+		if (file_exists($cmd) || strpos($cmd, '$') === 0) {
+			$ret = $cmd;
+		} else {
+			$resolveCmd = 'which ' . escapeshellarg($cmd);
+			$ret = trim(`$resolveCmd`);
+		}
+		return ($ret === '') ? false : $ret;
+	}
 }
---
+++ slothstars.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The WordPress Plugin Boilerplate.
+ *
+ * A foundation off of which to build well-documented WordPress plugins that
+ * also follow WordPress Coding Standards and PHP best practices.
+ *
+ * @author    Your Name <email@example.com>
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2014 Your Name or Company Name
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Sloth-Stars
+ * Plugin URI:        @TODO
+ * Description:       @TODO
+ * Version:           1.0.0
+ * Author:            @TODO
+ * Author URI:        @TODO
+ * Text Domain:       slothstars-locale
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/<owner>/<repo>
+ * WordPress-Plugin-Boilerplate: v2.6.1
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) die;
+
+/*----------------------------------------------------------------------------*
+ * Public-Facing Functionality
+ *----------------------------------------------------------------------------*/
+
+# register auto loader
+spl_autoload_register(function($class) { if (file_exists($filename = dirname(__FILE__) . '/lib/' . str_replace('\\', '/', ltrim($class, '\\')) . '.php')) require_once $filename; });
